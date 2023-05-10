db0affb942ecd1739ab100c13df18f0af39b9292
Cecília Serafim <ceciliaserafimti@gmail.com>
2023-05-10 09:22:22 -0300

    Adiicona requisito 2 class SimpleReport, onde tem Data de fabricação mais antiga, Data de validade mais próxima e Empresa com mais produtos

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,37 @@
+from datetime import datetime
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(list):
+        # old_manufacturing_date =
+        lista = []
+        lista_validade = []
+        data_atual = datetime.today().date()
+        empresas = []
+
+        for data in list:
+            lista.append(
+                datetime.strptime(
+                    data["data_de_fabricacao"], "%Y-%m-%d"
+                ).date()
+            )
+
+        for data in list:
+            date_time = datetime.strptime(
+                data["data_de_validade"], "%Y-%m-%d"
+            ).date()
+            if date_time > data_atual:
+                lista_validade.append(date_time)
+
+        for data in list:
+            empresas.append(data["nome_da_empresa"])
+
+        frequencia_empresa = Counter(empresas).most_common()
+        mais_frequente = frequencia_empresa[0]
+
+        return (f"Data de fabricação mais antiga: {min(lista)}\n"
+                f"Data de validade mais próxima: {min(lista_validade)}\n"
+                f"Empresa com mais produtos: {mais_frequente[0]}")
+    
